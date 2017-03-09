df0e657adafa5f52c303f91ff446ea2261b0cb4e
sergeybelov <sergeybelov@users.noreply.github.com>
2017-03-09 09:59:39 +0300

    Update Неделя 4. Линейная регрессия - прогноз оклада по описанию вакансии.py

--- Неделя 4. Линейная регрессия - прогноз оклада по описанию вакансии.py
+++ Неделя 4. Линейная регрессия - прогноз оклада по описанию вакансии.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Mar  8 11:06:10 2017
-
-
 """
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import DictVectorizer
 from scipy.sparse import hstack
+from sklearn.linear_model import Ridge
 
 #==============================================================================
 # Загрузите данные об описаниях вакансий и соответствующих годовых зарплатах из файла salary-train.csv
@@ -32,7 +31,8 @@ data_test['FullDescription'].replace('[^a-zA-Z0-9]', ' ', regex = True,inplace=T
 # Оставьте только те слова, которые встречаются хотя бы в 5 объектах (параметр min_df у TfidfVectorizer).
 #==============================================================================
 vectorizer = TfidfVectorizer(min_df=5)
-fullDescription_train=vectorizer.fit_transform(data_train['FullDescription'])
+fullDescription_train=vectorizer.fit_transform(data_train['FullDescription'])#сбор статистики и трансформация характетистик в числа
+fullDescription_test=vectorizer.transform(data_test['FullDescription'])#трансформация на основе ранее собранной статистики
 
 #==============================================================================
 # Замените пропуски в столбцах LocationNormalized и ContractTime на специальную строку 'nan'.
@@ -52,14 +52,20 @@ X_test_categ = enc.transform(data_test[['LocationNormalized', 'ContractTime']].t
 # Обратите внимание, что матрицы для текстов и категориальных признаков являются разреженными.
 # Для объединения их столбцов нужно воспользоваться функцией scipy.sparse.hstack.
 #==============================================================================
-joinedMatrix=hstack([X_train_categ,fullDescription_train])#соединяем признаки
+joinedMatrix_train=hstack([fullDescription_train,X_train_categ])#соединяем признаки
 
 #==============================================================================
 # Обучите гребневую регрессию с параметрами alpha=1 и random_state=241. Целевая переменная записана в столбце SalaryNormalized.
 #==============================================================================
-
+clf = Ridge(alpha=1.0,random_state=241)
+clf.fit(joinedMatrix_train, data_train['SalaryNormalized'])
 
 #==============================================================================
 # Постройте прогнозы для двух примеров из файла salary-test-mini.csv.
 # Значения полученных прогнозов являются ответом на задание. Укажите их через пробел.
 #==============================================================================
+joinedMatrix_test=hstack([fullDescription_test,X_test_categ])#соединяем признаки
+predict=clf.predict(joinedMatrix_test)
+
+print('Прогноз по зарплате')
+print (' '.join(map(lambda x: str(round(x,2)), predict)))
