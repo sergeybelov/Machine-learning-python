e7db12a0ed21f47f26d7f8878975a21bcde12fea
sergeybelov <mail@belovsergey.ru>
2017-03-14 23:24:32 +0300

    рабочий вариант

--- Неделя 5. Градиентный бустинг над решающими деревьями.py
+++ Неделя 5. Градиентный бустинг над решающими деревьями.py
@@ -11,6 +11,7 @@ from math import exp
 import numpy as np
 from sklearn.metrics import log_loss
 import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
 
 #==============================================================================
 # Загрузите выборку из файла gbm-data.csv с помощью pandas и преобразуйте ее в массив numpy (параметр values у датафрейма).
@@ -22,7 +23,7 @@ data_train=pd.read_csv('gbm-data.csv').values
 
 # Разбейте выборку на обучающую и тестовую, используя функцию train_test_split с параметрами test_size = 0.8 и random_state = 241.
 #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-X_train, X_test, y_train, y_test = train_test_split(data_train[:,:-1],data_train[:,:1].ravel(),test_size = 0.8,random_state = 241)
+X_train, X_test, y_train, y_test = train_test_split(data_train[:,1:],data_train[:,:1].ravel(),test_size = 0.8,random_state = 241)#X- начиная со второй колонки, Y - только первая колонка
 
 del data_train
 
@@ -40,16 +41,52 @@ def calc(X,y_true,n_est):
     return score
 
 
+
 learning_rate=[1, 0.5, 0.3, 0.2, 0.1]
 n_est=250
 for lr in learning_rate:
-    clf=GradientBoostingClassifier(n_estimators=n_est, verbose=False, random_state=241,learning_rate=lr)
+    clf=GradientBoostingClassifier(n_estimators=n_est, verbose=True, random_state=241,learning_rate=lr)
     clf.fit(X_train,y_train)
 
     train_score = calc(X_train,y_train,n_est)
     test_score = calc(X_test,y_test,n_est)
 
+    if lr==0.2: test_score02=test_score
+
     plt.figure()
     plt.plot(test_score, 'r', linewidth=3)
     plt.plot(train_score, 'g', linewidth=2)
     plt.legend(['test', 'train'])
+
+#Как можно охарактеризовать график качества на тестовой выборке, начиная с некоторой итерации: переобучение
+#(overfitting) или недообучение (underfitting)? В ответе укажите одно из слов overfitting либо underfitting.
+#Ответ - underfitting
+
+
+#==============================================================================
+# 4. Приведите минимальное значение log-loss на тестовой выборке и номер итерации, на котором оно достигается, при learning_rate = 0.2.
+#==============================================================================
+test_scorePD=pd.DataFrame(data=test_score02)
+test_scorePD.sort_values([0],inplace=True, ascending=True)
+test_scorePD[0]=test_scorePD[0].map(lambda x: round(x,2))
+print('Приведите минимальное значение log-loss на тестовой выборке и номер итерации, на котором оно достигается, при learning_rate = 0.2')
+val=test_scorePD.head(1)
+print(val)
+
+#==============================================================================
+# 5. На этих же данных обучите RandomForestClassifier с количеством деревьев, равным количеству итераций,
+# на котором достигается наилучшее качество у градиентного бустинга из предыдущего пункта, c random_state=241 и
+# остальными параметрами по умолчанию.
+#
+# Какое значение log-loss на тесте получается у этого случайного леса?
+# (Не забывайте, что предсказания нужно получать с помощью функции predict_proba.
+# В данном случае брать сигмоиду от оценки вероятности класса не нужно)
+#==============================================================================
+clf = RandomForestClassifier(n_estimators=val.index.values[0],random_state=241)#Конструктор случайного леса
+clf.fit(X_train,y_train)
+
+pred = clf.predict_proba(X_test)
+score=log_loss(y_test, pred)
+
+print('Какое значение log-loss на тесте получается у этого случайного леса?')
+print(round(score,2))
